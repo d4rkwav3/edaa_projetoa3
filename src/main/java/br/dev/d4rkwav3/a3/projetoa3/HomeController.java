37dbd8027d7229ac558517e53da0cbfb938e68a7
Bruno Venâncio de Souza e Silva <lostmellodies@gmail.com>
2024-06-08 23:30:42 -0300

    Reestruturação do projeto

--- src/main/java/br/dev/d4rkwav3/a3/projetoa3/HomeController.java
+++ src/main/java/br/dev/d4rkwav3/a3/projetoa3/HomeController.java
@@ -1,33 +1,18 @@
 package br.dev.d4rkwav3.a3.projetoa3;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-
 
 @Controller
 public class HomeController {
-    private final ObjetiveService objservice;
-
-    public HomeController(ObjetiveService objsvr) {
-        this.objservice = objsvr;
+    
+    public HomeController() {
+        
     }
 
     @GetMapping("/")
-    public String index(Model model) {
-        model.addAttribute("objetivos", objservice.getObjetives());
-
+    public String index() {
         return "index";
-    }
-
-    @PostMapping("/new-obj")
-    public String novoObjetivo(@ModelAttribute Objetive obj) {
-        objservice.create(obj);
-        
-        return "redirect:/";
-    }
-    
+    }   
     
 }
--- src/main/java/br/dev/d4rkwav3/a3/projetoa3/Objetive.java
+++
@@ -1,3 +0,0 @@
-package br.dev.d4rkwav3.a3.projetoa3;
-
-record Objetive(String description) {};
