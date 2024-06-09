8e18cf33b37d06f4c88953d4abc6c786033ee244
Bruno Venâncio de Souza e Silva <lostmellodies@gmail.com>
2024-06-09 16:41:11 -0300

    Substituição do Mustache pela Thymeleaf para processamento de templates (superior)

--- src/main/java/br/dev/d4rkwav3/a3/projetoa3/HomeController.java
+++ src/main/java/br/dev/d4rkwav3/a3/projetoa3/HomeController.java
@@ -1,6 +1,7 @@
 package br.dev.d4rkwav3.a3.projetoa3;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
@@ -10,19 +11,31 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class HomeController {
-    
-    public HomeController() {
-        
+
+    private String[] membros = {
+        "Bruno Venâncio de Souza e Silva, RA: 821135934",
+        "Henrick Melo Vital, RA: 821224905"
+    };
+
+    /**
+     * @return Array de strings com os nomes e ra dos membros do grupo
+     */
+    public String[] getMembros() {
+        return membros;
     }
+
+    public HomeController() {}
 
     /**
      * Retorna a home page ao acessar o endereço raiz
      * 
-     * 
-     * @return Retorna o template index.mustache
+     * @param Model adiciona um atributo chamado membros a partir 
+     * do array de membros da classe através do método getMembros.
+     * @return Retorna o template index.html
      */
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("membros", getMembros());
         return "index";
     }   
     
