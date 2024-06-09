20d40d9b85e2d4a644bfc643a9a79b4d1762529a
Bruno Venâncio de Souza e Silva <lostmellodies@gmail.com>
2024-06-09 00:22:45 -0300

    Criação do JavaDoc nas classes existentes

--- src/main/java/br/dev/d4rkwav3/a3/projetoa3/HomeController.java
+++ src/main/java/br/dev/d4rkwav3/a3/projetoa3/HomeController.java
@@ -3,6 +3,11 @@ package br.dev.d4rkwav3.a3.projetoa3;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * Classe responsável pela página inicial do projeto
+ * <p>Todas as requisições Rest da página inicial são feitas aqui</p>
+ * @author Bruno Venâncio
+ */
 @Controller
 public class HomeController {
     
@@ -10,6 +15,12 @@ public class HomeController {
         
     }
 
+    /**
+     * Retorna a home page ao acessar o endereço raiz
+     * 
+     * 
+     * @return Retorna o template index.mustache
+     */
     @GetMapping("/")
     public String index() {
         return "index";
--- src/main/java/br/dev/d4rkwav3/a3/projetoa3/Projetoa3Application.java
+++ src/main/java/br/dev/d4rkwav3/a3/projetoa3/Projetoa3Application.java
@@ -3,9 +3,19 @@ package br.dev.d4rkwav3.a3.projetoa3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Classe de execução do SpringBoot
+ * Execute essa classe para dar inicio a aplicação
+ * @author Bruno Venâncio
+ */
 @SpringBootApplication
 public class Projetoa3Application {
 
+	/**
+	 * Metódo de execução da aplicação
+	 * @param args Argumentos extras para a aplicação, desnecessário pois nenhum
+	 * argumento é processado.
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(Projetoa3Application.class, args);
 	}
